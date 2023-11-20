85f8218174fe1a6fe93c00751af62ab872bd1245
Chisom Mgbedike <you@example.com>
2023-11-20 00:29:28 +0000

    fixed issue with scraping and made variables more generic

--- scraper_functions.py
+++ scraper_functions.py
@@ -2,73 +2,65 @@ from selenium import webdriver
 
 from selenium.webdriver.common.by import By
 
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
-import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
 
 from selenium.webdriver.chrome.options import Options
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+
+
 
 tracker_columns = ["Shop", "Product", "In Stock", "Price"]
 
 def create_driver_instance():
     options = Options()
-    options.add_argument('--headless=new')
+    # options.add_argument('--headless=new') - can't accept cookies when this is enabled
 
     driver_instance = webdriver.Chrome(options=options)
-    driver_instance.implicitly_wait(1)
+    driver_instance.implicitly_wait(3)
     return driver_instance
 
 
 # Shop: Superdrug
-def superdrug_price_check(url, product_name):
+def superdrug_price_check(url):
     browser = create_driver_instance()
     browser.get(url)
 
+    cookies_button = browser.find_element(By.ID, "onetrust-accept-btn-handler")
+    cookies_button.click()
+
     try:
-        browser.find_element(By.XPATH, "//button[@class='button--primary.button']/span[contains(text(), 'Notify when in stock')]")
-        superdrug_data = pd.DataFrame([("Superdrug", product_name, "N","N/A")], columns=tracker_columns)
+        browser.find_element(By.XPATH, "//span[contains(text(), 'Notify when in stock')]")
     except NoSuchElementException:
-        product_price = WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='price__current']")))
-        superdrug_data = pd.DataFrame([("Superdrug", product_name, "Y", product_price.text)], columns=tracker_columns)
+        product_price = browser.find_element(By.XPATH, "//span[@class='price__current']")
+        product_title = browser.find_element(By.XPATH, "//h1[@class='product-details-title__text']")
+        superdrug_data = pd.DataFrame([("Superdrug", product_title.text, "Y", product_price.text)], columns=tracker_columns)
+    else:
+        product_title = browser.find_element(By.XPATH, "//h1[@class='product-details-title__text']")
+        superdrug_data = pd.DataFrame([("Superdrug", product_title.text, "N","N/A")], columns=tracker_columns)
     finally:
         browser.quit()
         return superdrug_data
     
 
-# Shop: Wilko
-def wilko_price_check(url, product_name):
-    
-    browser = create_driver_instance()
-    browser.get(url)
-
-    try:
-        WebDriverWait(browser, 300).until(EC.visibility_of_element_located((By.XPATH, "//h2[contains(text(), 'out of stock')]")))
-        wilko_data = pd.DataFrame([("Wilko", product_name, "N","N/A")], columns=tracker_columns)
-    except NoSuchElementException or TimeoutException:
-        product_price_wilko = WebDriverWait(browser, 60).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'pdp-price')]")))
-        wilko_data = pd.DataFrame([("Wilko", product_name, "Y", product_price_wilko.text)], columns=tracker_columns)
-    finally:
-        browser.quit()
-        return wilko_data
-    
-
 # Shop: Boots
-def boots_price_check(url, product_name):
+def boots_price_check(url):
 
     browser = create_driver_instance()
     browser.get(url)
+
+    cookies_button = browser.find_element(By.ID, "onetrust-accept-btn-handler")
+    cookies_button.click()
 
     try:
         browser.find_element(By.ID, "add2CartBtn") # only present when the item is in stock
     except NoSuchElementException:
-        boots_data = pd.DataFrame([("Boots", product_name, "N","N/A")], columns=tracker_columns)
+        product_title = browser.find_element(By.ID, "estore_product_title")
+        boots_data = pd.DataFrame([("Boots", product_title.text, "N","N/A")], columns=tracker_columns)
     else:
         product_price_boots = browser.find_element(By.ID, "PDP_productPrice")
-        boots_data = pd.DataFrame([("Boots", product_name, "Y", product_price_boots.text)], columns=tracker_columns)
+        product_title = browser.find_element(By.ID, "estore_product_title")
+        boots_data = pd.DataFrame([("Boots", product_title.text, "Y", product_price_boots.text)], columns=tracker_columns)
     finally:
         browser.quit()
         return boots_data
