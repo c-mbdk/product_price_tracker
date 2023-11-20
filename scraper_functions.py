e31ada3b88670c3697ad5387251f39837132cb79
Chisom Mgbedike <you@example.com>
2023-11-20 21:22:38 +0000

    update config

--- scraper_functions.py
+++ scraper_functions.py
@@ -23,10 +23,10 @@ def create_driver_instance():
     # options.add_argument('--headless=new') - can't accept cookies when this is enabled
     options.add_argument('--no-sandbox')
     options.add_argument('--remote-debugging-port=9222')
-    # options.binary_location = "/opt/hostedtoolcache/chromium/latest/x64/chrome"
-    # service = Service(executable_path=binary_path)
+    options.binary_location = "/usr/bin/google-chrome"
+    service = Service(executable_path=binary_path)
 
-    driver_instance = webdriver.Chrome(options=options)
+    driver_instance = webdriver.Chrome(options=options, service=service)
     driver_instance.implicitly_wait(3)
     return driver_instance
 
